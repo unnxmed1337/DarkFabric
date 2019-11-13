c39fc97f66b1375dbb9945817ada8e0c379e8564
Hexeption <minecraftfun201@gmail.com>
2019-11-13 18:15:40 +0000

    :bug: Fixed being able to toggle mods in chat and other ui's

--- src/main/java/uk/co/hexeption/darkforgereborn/event/EventCaller.java
+++ src/main/java/uk/co/hexeption/darkforgereborn/event/EventCaller.java
@@ -5,6 +5,7 @@ import me.zero.alpine.listener.Listenable;
 import me.zero.alpine.listener.Listener;
 import org.lwjgl.glfw.GLFW;
 import uk.co.hexeption.darkforgereborn.DarkForgeReborn;
+import uk.co.hexeption.darkforgereborn.IMC;
 import uk.co.hexeption.darkforgereborn.event.events.input.EventKeyPressed;
 import uk.co.hexeption.darkforgereborn.mod.options.Option;
 
@@ -14,12 +15,12 @@ import uk.co.hexeption.darkforgereborn.mod.options.Option;
  * @author Hexeption admin@hexeption.co.uk
  * @since 12/11/2019 - 11:37 am
  */
-public class EventCaller implements Listenable {
+public class EventCaller implements Listenable, IMC {
 
     @EventHandler
     private Listener<EventKeyPressed> eventKeyPressedListener = new Listener<>(event -> {
         DarkForgeReborn.INSTANCE.modManager.getMods().forEach(mod -> {
-            if (GLFW.glfwGetKeyName(event.getKey(), event.getScancode()) == null) {
+            if (GLFW.glfwGetKeyName(event.getKey(), event.getScancode()) == null || mc.currentScreen != null) {
                 return;
             }
             if (GLFW.glfwGetKeyName(event.getKey(), event.getScancode()).equalsIgnoreCase(Option.get(mod.options, "keybind").STRING())) {
