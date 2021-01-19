e8e61efe7b8e9bceb638a8d35673dfd003469e96
xVoiceSyntax <52635610+xVoiceSyntax@users.noreply.github.com>
2021-01-19 13:41:42 +0100

    Improved Registry.java

--- src/main/java/com/github/darkfabric/base/Registry.java
+++ src/main/java/com/github/darkfabric/base/Registry.java
@@ -1,32 +1,42 @@
 package com.github.darkfabric.base;
 
-import lombok.Getter;
-
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public abstract class Registry<V> {
 
-    @Getter
-    private final List<V> objects = new CopyOnWriteArrayList<>();
-
-    public void register(V object) {
-        objects.add(object);
+    private final List<V> objects;
+    
+    public Registry(List<V> objects) {
+        this.objects = objects;
+    }
+    
+    public Registry() {
+        this(new CopyOnWriteArrayList<>(););
+    }
+    
+    public void register(V... objects) {
+        objects.addAll(Arrays.asList(objects));
     }
 
-    public void unregister(V object) {
-        objects.remove(object);
+    public void unregister(V objects) {
+        objects.removeAll(Arrays.asList(objects));
+    }
+    
+        public void clearList() {
+        objects.clear();
     }
 
-    public V getByClass(Class<?> clazz) {
-        for (V object : this.getObjects())
-            if (object.getClass() == clazz)
-                return object;
-        return null;
+    public <E extends V> getByClass(Class<?> clazz) {
+        return this.getBy(bject -> object.getClass() == clazz);
     }
-
-    public void unregisterAllRegisteredObjects() {
-        objects.forEach(this::unregister);
+    
+    public <E extends V> getBy(Predicate<V> filter) {
+        return (E) this.getObjects().stream().filter(filter).findFirst.orElse(null);
+    }
+    
+    public List<V> getObjects() {
+        return Collections.unmodifiableList(this.objects);
     }
 
 }
